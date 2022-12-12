d5ef5fd42216931aab48a58b71f49f9840266c50
Felix <sze.chan1996@gmail.com>
2022-12-12 16:08:14 +1100

    Create barplot.py

---
+++ barplot.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv(r'C:\Users\szech\Desktop\sampleclient.csv') 
+s = df.groupby('cut').size()
+s.plot(
+    kind = 'bar',
+)
+plt.savefig('plot.png') 
+plt.show() 
