5e8ba13ed59d759f6cfeb4ec5d9e37c89c069e3e
Felix <sze.chan1996@gmail.com>
2022-12-12 15:56:53 +1100

    Update sampleclientpiegraph.py

--- sampleclientpiegraph.py
+++ sampleclientpiegraph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt 
 df = pd.read_csv(r'C:\Users\szech\Desktop\sampleclient.csv')
-s = df.groupby('Campaign Name')['Total Profit ($)'].mean()
+s = df.groupby('Campaign Name')['Total Profit ($)'].mean() #Find the mean profit of each campaign
 s.plot(
     kind = 'pie'
 )
