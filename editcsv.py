8394f58e18b864ae3efdaf111e334889acc6626b
Felix <sze.chan1996@gmail.com>
2022-12-12 11:30:12 +1100

    Update editcsv.py

--- editcsv.py
+++ editcsv.py
@@ -1,2 +1,9 @@
 #Edit the CSV file within the folder: 
 
+from csv import writer
+List = ['G7',1240,17]
+with open('students.csv','a') as f_object:
+    writer_object=writer(f_object)
+    writer_object.writerow(List)
+    f_object.close()    
+    
