7b79e230e3faf44bfb579965e83ddd0814817321
Felix <sze.chan1996@gmail.com>
2022-12-12 12:39:14 +1100

    Create editcsv1.py

---
+++ editcsv1.py
@@ -0,0 +1,11 @@
+#Another way to append rows to csv in folder (when newdata is same for multiple rows): 
+
+import csv 
+
+counter = range(3)
+
+with open('students.csv', 'a', newline='') as graphFile:
+    graphFileWriter = csv.writer(graphFile)
+    for x in counter:
+        graphFileWriter.writerow(['Withdrawn','Withdrawn','Withdrawn','Withdrawn'])
+ 
