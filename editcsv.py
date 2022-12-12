4b920683ce1d3ab5daf8c805897b037cebd65fec
Felix <sze.chan1996@gmail.com>
2022-12-12 11:49:58 +1100

    Update editcsv.py

--- editcsv.py
+++ editcsv.py
@@ -2,7 +2,7 @@
 
 from csv import writer
 List = ['Joseph',22,'Computer Science','Data Science']
-with open('machinesdata.csv','a') as f_object:
+with open('students.csv','a') as f_object:
     writer_object=writer(f_object)
     writer_object.writerow(List)
     f_object.close()    
