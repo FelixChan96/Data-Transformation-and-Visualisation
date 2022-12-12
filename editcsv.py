0a7d9fe405a5df6dd5b77a953a4a4b0bb52d5301
Felix <sze.chan1996@gmail.com>
2022-12-12 11:51:10 +1100

    Update editcsv.py

--- editcsv.py
+++ editcsv.py
@@ -5,5 +5,5 @@ List = ['Joseph',22,'Computer Science','Data Science']
 with open('students.csv','a') as f_object:
     writer_object=writer(f_object)
     writer_object.writerow(List)
-    f_object.close()    
+    f_object.close()     
     
