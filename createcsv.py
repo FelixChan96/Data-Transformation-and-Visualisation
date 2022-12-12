52c23fe850f76ce20f83d5fe366886565d3b061f
Felix <sze.chan1996@gmail.com>
2022-12-12 11:46:48 +1100

    Create createcsv.py

---
+++ createcsv.py
@@ -0,0 +1,16 @@
+#creating csv file in folder: 
+import csv
+
+student_header = ['name', 'age', 'major', 'minor']
+
+student_data = [
+    {'name': 'Jack', 'age': 23, 'major': 'Physics', 'minor': 'Chemistry'},
+    {'name': 'Sophie', 'age': 22, 'major': 'Physics', 'minor': 'Computer Science'},
+    {'name': 'John', 'age': 24, 'major': 'Mathematics', 'minor': 'Physics'},
+    {'name': 'Jane', 'age': 30, 'major': 'Chemistry', 'minor': 'Physics'}
+]
+
+with open('students.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=student_header)
+    writer.writeheader()
+    writer.writerows(student_data)
