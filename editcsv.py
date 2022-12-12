d31347763ca54dc019f0025e0bfc3970b8c94e5e
Felix <119918540+FelixChan96@users.noreply.github.com>
2022-12-12 11:45:29 +1100

    Delete editcsv.py

--- editcsv.py
+++
@@ -1,9 +0,0 @@
-#Edit the CSV file within the folder: 
-
-from csv import writer
-List = ['Samantha',26,'Computer Science', 'Data Science']
-with open('students.csv','a') as f_object:
-    writer_object=writer(f_object)
-    writer_object.writerow(List)
-    f_object.close()    
-    
