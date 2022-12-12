ee05962991e5cf7be0a8f94e468d7ef890587849
Felix <sze.chan1996@gmail.com>
2022-12-12 16:04:11 +1100

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 df = pd.read_csv(r'C:\Users\szech\Desktop\sampleclient.csv') 
 df.plot(
     kind = 'scatter',
-    x = 'Total Cost ($)',
+    x = 'Postcode',
     y = 'Total Profit ($)',
     marker = '.', # Set the shape of the markers
 )
 plt.savefig('plot.png')  
-plt.show() 
+plt.show()  
