65b0932b097543e8f2a91d48518b47eb92a7a118
Felix <sze.chan1996@gmail.com>
2022-12-14 11:16:50 +1100

    Update NDIS_transformation&visualisation.py

--- NDIS_transformation&visualisation.py
+++ NDIS_transformation&visualisation.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
-df = pd.read_csv(r'C:\Users\szech\Desktop\sampleclient.csv')   
+df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')   
