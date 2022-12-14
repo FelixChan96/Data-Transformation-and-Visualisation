21435633e18938b973cf764f758be53253a1ad3c
Felix <sze.chan1996@gmail.com>
2022-12-14 13:44:13 +1100

    Update NDIS_transformation&visualisation.py

--- NDIS_transformation&visualisation.py
+++ NDIS_transformation&visualisation.py
@@ -13,8 +13,8 @@ df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')
 #print(df_first_rows)
 
 #Now to start editing the csv file. First, remove duplicates of the same registered provider name, only one office is required:
-#df = df.drop_duplicates(subset=['Registered Provider Name'], inplace=False)
-#df = df.to_csv("registered_providers_NDIS.csv", index=False)
+df = df.drop_duplicates(subset=['Registered Provider Name'], inplace=False)
+df = df.to_csv("registered_providers_NDIS.csv", index=False)
 
 #Now to separate the address string to retrieve postcode: 
 df["Postcode"] = df["Address"].str.extract("(\d*\.?\d+)", expand=True)
