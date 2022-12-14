de3d2dd45d70f9241b83519171cbf5940eba9150
Felix <sze.chan1996@gmail.com>
2022-12-14 11:21:39 +1100

    Update NDIS_transformation&visualisation.py

--- NDIS_transformation&visualisation.py
+++ NDIS_transformation&visualisation.py
@@ -1,4 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
-df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')   
+import seaborn
+df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')    
