6bc533906c13dbf8fd63b5e8c67287180d5cd4a9
Felix <sze.chan1996@gmail.com>
2022-12-14 14:49:01 +1100

    Create NDISpie.py

---
+++ NDISpie.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import csv
+import matplotlib.pyplot as plt 
+df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')
+s = df.groupby('State').mean() #Find the mean profit of each campaign
+s.plot(
+    kind = 'pie'
+)
+plt.savefig('NDISprovidersbystate.png')   
+plt.show() 
