40470c1208d7ea47579c9d221e25cf1e46f3da4c
Felix <sze.chan1996@gmail.com>
2022-12-14 14:52:32 +1100

    Update NDISpie.py

--- NDISpie.py
+++ NDISpie.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt 
 df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')
-s = df.groupby('State').mean() #Find the mean profit of each campaign
+s = df.groupby('State')['Total Profit ($)'].mean() #Find the mean profit of each campaign
 s.plot(
     kind = 'pie'
 )
