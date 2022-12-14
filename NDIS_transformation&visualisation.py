a5e84ef543e515a92f37e2356d837206041a7f26
Felix <sze.chan1996@gmail.com>
2022-12-14 14:31:06 +1100

    Update NDIS_transformation&visualisation.py

--- NDIS_transformation&visualisation.py
+++ NDIS_transformation&visualisation.py
@@ -13,12 +13,12 @@ df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')
 #print(df_first_rows)
 
 #Now to start editing the csv file. First, remove duplicates of the same registered provider name, only one office is required:
-df = df.drop_duplicates(subset=['Registered Provider Name'], inplace=False)
+df = df.drop_duplicates(subset=['Registered Provider Name'], inplace=True)
 df = df.to_csv("registered_providers_NDIS.csv", index=False)
 
 #To get only the active providers: 
 
-df_new = df.drop(df[(df['Active Provider (payment received last 3 months)'] == 1.0)].index)
+df_new = df.drop(df[(df['Active Provider (payment received last 3 months)'] == 1)].index)
 print(df_new)
 
 
