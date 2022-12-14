1f79f8f04a7a04fcf0adf733037af51609c5c18f
Felix <sze.chan1996@gmail.com>
2022-12-14 12:41:44 +1100

    Update NDIS_transformation&visualisation.py

--- NDIS_transformation&visualisation.py
+++ NDIS_transformation&visualisation.py
@@ -13,7 +13,5 @@ df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')
 #print(df_first_rows)
 
 #Now to start editing the csv file. First, remove duplicates of the same registered provider name, only one office is required:
-
-df = df.drop_duplicates(subset=['Phone', 'Email','Website'], inplace=False)
-
+df = df.drop_duplicates(subset=['Registered Provider Name'], inplace=False)
 df = df.to_csv("registered_providers_NDIS.csv", index=False)
