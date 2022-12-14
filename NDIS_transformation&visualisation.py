5576e0062947ee39ad28103ae76346e89dece220
Felix <sze.chan1996@gmail.com>
2022-12-14 12:11:22 +1100

    Update NDIS_transformation&visualisation.py

--- NDIS_transformation&visualisation.py
+++ NDIS_transformation&visualisation.py
@@ -3,5 +3,11 @@ import matplotlib.pyplot as plt
 import csv
 import seaborn
 df = pd.read_csv(r'C:\Users\szech\Desktop\registered_providers_NDIS.csv')    
-#for col in df.columns:
-    #print(col)
+
+#Checking column headers: 
+for col in df.columns:
+    print(col)
+
+#Checking the first 5 rows of data: 
+df_first_rows = df.head(5)
+print (df_first_rows)
