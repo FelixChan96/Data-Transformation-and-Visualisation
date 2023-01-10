826470d275a0bc2a4abcf22750eadd53a29d3776
Felix <sze.chan1996@gmail.com>
2023-01-10 20:39:22 +1100

    Update etherscanAPIdata.py

--- etherscanAPIdata.py
+++ etherscanAPIdata.py
@@ -3,7 +3,7 @@ import json
 
 #establishing a secure connection: 
 
-response = requests.get("https://api.etherscan.io/api?module=account&action=balancemulti&address=0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a,0x63a9975ba31b0b9626b34300f7f627147df1f526,0x198ef1ec325a96cc354c7266a038be8b5c558f67&tag=latest&apikey=N6RXSV29BTHWDIX4Y3YVS78T1A2PPX7Y7I").json()
+response = requests.get("https://api.data.abs.gov.au/data/ABS,C21_G13_SAL,1.0.0/...10891..?startPeriod=2021&dimensionAtObservation=AllDimensions").json()
 print(response.status_code)   
  
 #Retrieving sample data from API:
