675854830e1f9158401842115b0b3d578b444009
Felix <sze.chan1996@gmail.com>
2023-01-10 22:16:07 +1100

    Update alternateetherscanAPI.py

--- alternateetherscanAPI.py
+++ alternateetherscanAPI.py
@@ -4,6 +4,16 @@ with urllib.request.urlopen("https://api.data.abs.gov.au/data/ABS,C21_G13_SAL,1.
     s = url.read()
     print(s)
 
+import json
+
+if response.status_code == 200:
+  data = response.json()
+  with open('testABS.json','w') as f:
+    json.dump(data,f, indent=2)
+
+else:
+  print('Failed to load data from API')
+
      
 
 
