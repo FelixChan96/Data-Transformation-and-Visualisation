572da8d4dbbc976b7e59e472f063b0a070265a57
Felix <sze.chan1996@gmail.com>
2023-01-11 15:17:24 +1100

    Update etherscanAPIdata.py

--- etherscanAPIdata.py
+++ etherscanAPIdata.py
@@ -2,18 +2,8 @@ import requests
 import json
 
 #establishing a secure connection: 
-
-response = requests.get("https://api.data.abs.gov.au/data/ABS,C21_G13_SAL,1.0.0/...10891..?startPeriod=2021&dimensionAtObservation=AllDimensions").json()
-print(response.status_code)   
+url = "https://api.data.abs.gov.au/data/C21_G09_SAL/all"
+response = requests.get(url,timeout=1000000)
+data = response.json()
+print(response)
  
-#Retrieving sample data from API:
-
-data = response.text
-
-#Parse the sample data into JSON format: 
-
-parse_json = json.loads(data)
-
-#print the sample data: 
-
-print(parse_json) 
